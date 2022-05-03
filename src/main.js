45ec160ba5ad96c9efc9911548ec107206e9c377
davidyoon <qqpo12@naver.com>
2022-05-03 22:29:07 +0900

    image server

---
+++ src/main.js
@@ -0,0 +1,52 @@
+// @ts-check
+require('dotenv').config()
+// const fs = require('fs')
+const http = require('http')
+const { createApi } = require('unsplash-js')
+const { default: fetch } = require('node-fetch')
+
+const unsplash = createApi({
+  accessKey: process.env.UNSPLASH_API_ACCESS_KEY,
+  // @ts-ignore
+  fetch,
+})
+
+/**
+ *
+ * @param {string} query
+ */
+
+async function searchImage(query) {
+  const result = await unsplash.search.getPhotos({ query })
+
+  if (!result.response) {
+    throw new Error('Failed to search image.')
+  }
+
+  const image = result.response.results[0]
+
+  if (!image) {
+    throw new Error('No image found.')
+  }
+
+  return {
+    description: image.description || image.alt_description,
+    url: image.urls.regular,
+  }
+}
+
+const server = http.createServer((req, res) => {
+  async function main() {
+    const result = await searchImage('mountain')
+    const resp = await fetch(result.url)
+    resp.body.pipe(res)
+  }
+
+  main()
+})
+
+const PORT = 5005
+
+server.listen(PORT, () => {
+  console.log(`The server is listening at port ${PORT}`)
+})

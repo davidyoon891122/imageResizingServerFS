5b12051e9cfff964bb04162812afaaa9dd52c78e
davidyoon <qqpo12@naver.com>
2022-05-05 21:25:57 +0900

    add cashing, add error handling

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 require('dotenv').config()
 // const fs = require('fs')
 const http = require('http')
+const path = require('path')
+const fs = require('fs')
 const { createApi } = require('unsplash-js')
 const { default: fetch } = require('node-fetch')
 const sharp = require('sharp')
@@ -13,10 +15,8 @@ const unsplash = createApi({
 })
 
 /**
- *
  * @param {string} query
  */
-
 async function searchImage(query) {
   const result = await unsplash.search.getPhotos({ query })
 
@@ -35,12 +35,56 @@ async function searchImage(query) {
     url: image.urls.regular,
   }
 }
+/**
+ * 이미지를 Unsplash에서 검색하거나, 이미 있다면 캐시된 이미지를 불러온다.
+ * @param {string} query
+ */
+async function getCashedImageOrSearchedImage(query) {
+  const imageFilePath = path.resolve(__dirname, `../images/${query}`)
+
+  if (fs.existsSync(imageFilePath)) {
+    return {
+      message: `Returning cashed image: ${query}`,
+      stream: fs.createReadStream(imageFilePath),
+    }
+  }
+
+  const result = await searchImage(query)
+  const response = await fetch(result.url)
+
+  response.body.pipe(fs.createWriteStream(imageFilePath))
+
+  return {
+    message: `Returning new image: ${query}`,
+    stream: fs.createReadStream(imageFilePath),
+  }
+}
+
+/**
+ *
+ * @param {string} url
+ */
+function getQueryStringFromUrl(url) {
+  return url.slice(1)
+}
 
 const server = http.createServer((req, res) => {
   async function main() {
-    const result = await searchImage('mountain')
-    const resp = await fetch(result.url) // Todo: What is the fetch
-    resp.body.pipe(res)
+    if (!req.url || req.url.length < 2) {
+      res.statusCode = 400
+      res.end('Needs URL.')
+      return
+    }
+
+    const query = await getQueryStringFromUrl(req.url)
+    try {
+      const { message, stream } = await getCashedImageOrSearchedImage(query)
+      console.log(message)
+      stream.pipe(res)
+    } catch {
+      res.statusCode = 400
+      res.end()
+    }
   }
 
   main()
